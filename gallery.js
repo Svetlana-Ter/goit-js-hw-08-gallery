954215f5795df983be574bdcea91576f9d1e7b37
Svetlana <ternovskaya.svetlana89@gmail.com>
2020-10-11 14:11:37 +0300

    Добавляет переключение картинок

--- gallery.js
+++ gallery.js
@@ -5,22 +5,22 @@ const modalRef = document.querySelector('.js-lightbox');
 const originalImgRef = document.querySelector('.lightbox__image');
 const btnCloseRef = document.querySelector('button[data-action="close-lightbox"]');
 const overlayRef = document.querySelector('.lightbox__overlay');
-const btnLeftRef = document.querySelector('button[data-action="move-left"]');
-const btnRightRef = document.querySelector('button[data-action="move-right"]');
-// const currentImgRef = document.querySelectorAll('.gallery__image');
+const btnPrevRef = document.querySelector('button[data-action="move-prev"]');
+const btnNextRef = document.querySelector('button[data-action="move-next"]');
 
-const imagesHtml = images.map(({ preview, original, description }) => {
+const imagesHtml = images.map((image) => {
     return `
     <li class="gallery__item">
         <a
            class="gallery__link"
-           href="${original}"           
+           href="${image.original}"           
         >
             <img
               class="gallery__image"
-              src="${preview}"
-              data-source="${original}"
-              alt="${description}"
+              src="${image.preview}"
+              data-source="${image.original}"
+              data-index="${images.indexOf(image)}"
+              alt="${image.description}"
             />
         </a>
     </li>
@@ -34,8 +34,8 @@ galleryRef.addEventListener('click', onOriginalOpen);
 btnCloseRef.addEventListener('click', onOriginalClose);
 overlayRef.addEventListener('click', onOriginalClose);
 window.addEventListener('keydown', onOriginalCloseEsc);
-btnLeftRef.addEventListener('click', onImageMoveLeft);
-// btnRightRef.addEventListener('click', onImageMoveRight);
+btnPrevRef.addEventListener('click', onImageShowPrev);
+btnNextRef.addEventListener('click', onImageShowNext);
 
 function onOriginalOpen(event) {
     if (event.target.nodeName !== 'IMG') {
@@ -49,11 +49,11 @@ function onOriginalOpen(event) {
     
     const imageUrl = event.target.dataset.source;
     const alt = event.target.alt;    
+    const index = event.target.dataset.index;
 
     originalImgRef.src = imageUrl;
     originalImgRef.alt = alt;
-    
-
+    originalImgRef.dataset.index = index;    
 }
 
 function onOriginalClose() {
@@ -70,11 +70,26 @@ function onOriginalCloseEsc(ev) {
     onOriginalClose();
 }
 
-
-function onImageMoveLeft() {
-   
-   console.log(event.target); 
+function onImageShowPrev() {
+    let index = Number(originalImgRef.dataset.index);
+    if (index === 0) {
+        index = images.length - 1;
+    } else {
+       index -= 1;
+    }       
+    originalImgRef.src = images[index].original;
+    originalImgRef.alt = images[index].description;
+    originalImgRef.dataset.index = index;
 }
 
-
-
+function onImageShowNext() {
+    let index = Number(originalImgRef.dataset.index);
+     if (index === images.length - 1) {
+        index = 0;
+    } else {
+        index += 1;  
+    }      
+    originalImgRef.src = images[index].original;
+    originalImgRef.alt = images[index].description;
+    originalImgRef.dataset.index = index;
+}
